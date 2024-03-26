d134129169073bcea6016cbf47f9ad4e052b8e4d
Jayaprakash R <117837924+jpdevopstraining@users.noreply.github.com>
2024-03-26 11:17:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@
 docker login -u jayaprakashspt -p dckr_pat_oGtSwAHvSojOFRFNd_Wct4Ep7n4
 # Docker image details
 
-docker tag bannu jayaprakashspt/prod
-docker push jayaprakashspt/prod
+docker tag bannu jayaprakashspt/main
+docker push jayaprakashspt/main
 
 
 
