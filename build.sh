76a0a9940e532be466980d5779e2f0aa23adb5ab
Jayaprakash R <117837924+jpdevopstraining@users.noreply.github.com>
2024-03-26 10:44:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,17 +8,14 @@ echo "Building Docker images..."
 # Replace 'your_dockerfile_path' with the paths to the Dockerfiles
 # You can add more lines as needed for additional images
 
-IMAGE_1="your_image_name_1"
-DOCKERFILE_1="your_dockerfile_path_1"
+IMAGE_1="bannu"
 
-IMAGE_2="your_image_name_2"
-DOCKERFILE_2="your_dockerfile_path_2"
 
 # Build the first Docker image
-docker build -t $IMAGE_1 -f $DOCKERFILE_1 .
+docker build -t $IMAGE_1  .
 
 # Build the second Docker image
-docker build -t $IMAGE_2 -f $DOCKERFILE_2 .
+
 
 # You can add more build commands for additional images if needed
 
