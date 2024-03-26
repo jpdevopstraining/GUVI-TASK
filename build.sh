e06325d916de7d098e751d98488c2e731f06a1d4
Jayaprakash R <117837924+jpdevopstraining@users.noreply.github.com>
2024-03-26 11:11:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Build Docker images
-echo "Building Docker images..."
 
-# Define your Docker image names and paths
-# Replace 'your_image_name' with the desired image names
-# Replace 'your_dockerfile_path' with the paths to the Dockerfiles
-# You can add more lines as needed for additional images
 
-IMAGE_1="your_image_name_1"
-DOCKERFILE_1="your_dockerfile_path_1"
+# Server SSH details
 
-IMAGE_2="your_image_name_2"
-DOCKERFILE_2="your_dockerfile_path_2"
+docker login -u jayaprakashspt -p dckr_pat_oGtSwAHvSojOFRFNd_Wct4Ep7n4
+# Docker image details
 
-# Build the first Docker image
-docker build -t $IMAGE_1 -f $DOCKERFILE_1 .
-
-# Build the second Docker image
-docker build -t $IMAGE_2 -f $DOCKERFILE_2 .
-
-# You can add more build commands for additional images if needed
-
-echo "Docker images built successfully."
+docker tag bannu jayaprakashspt/prod
+docker push jayaprakashspt/prod
 
