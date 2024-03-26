f461e2e99d87187cce7e0043bd0298f5d08301ab
Jayaprakash R <117837924+jpdevopstraining@users.noreply.github.com>
2024-03-26 11:43:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Set your Docker Hub username
+DOCKER_USERNAME="jayaprakashspt"
+# Set your Docker Hub password
+DOCKER_PASSWORD="dckr_pat_Q250SXEkkRsQGSm0sYiInF3yiOQ"
+# Set the name for your Docker image
+IMAGE_NAME="bannu"
+# Set the tag for your Docker image
+TAG="v2"
+
+# Build the Docker image
+docker build -t jayaprakashspt/bannu:v2 .
+
+# Log in to Docker Hub
+echo "dckr_pat_Q250SXEkkRsQGSm0sYiInF3yiOQ" | docker login -u "jayaprakashspt" Jpmanjula@2024
+
+# Push the Docker image to Docker Hub
+docker push jayaprakashspt/bannu:v2
 
 
-# Server SSH details
 
-docker login -u jayaprakashspt -p dckr_pat_oGtSwAHvSojOFRFNd_Wct4Ep7n4
-# Docker image details
-
-docker tag bannu jayaprakashspt/main
-docker push jayaprakashspt/main
 
 
 
